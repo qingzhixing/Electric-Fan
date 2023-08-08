980f5e67e778ba49d76b703ebc7da6561f93e79a
qingzhixing <1418390991@qq.com>
2023-08-08 16:13:30 +0800

    提取通用类型

--- src/main/java/qingzhixing/utilities/MessageTool.java
+++ src/main/java/qingzhixing/utilities/MessageTool.java
@@ -7,7 +7,7 @@ import net.mamoe.mirai.message.data.SingleMessage;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.jetbrains.annotations.NotNull;
-
+import java.util.ArrayList;
 import java.util.List;
 
 public final class MessageTool {
@@ -19,30 +19,32 @@ public final class MessageTool {
      * @return 提取之后的 PlainText List,失败返回null
      */
     public static List<PlainText> ExtractPlainText(@NotNull Message message) {
-
+        return ExtractSingleMessageType(message,PlainText.class);
     }
 
     /*
-     * 从 message 中提取 T类型的 Message 并返回一个 List
+     * 从 message 中提取 SingleMessageType 类型的 Message 并返回一个 List
      * @param message 收到的Message
      * @return 提取之后的List ,失败返回null
      */
-    public static <T implements SingleMessage> List<T> ExtractSingleMessageType (@NotNull Message message) {
+    public static <T extends SingleMessage> List<T> ExtractSingleMessageType(@NotNull Message message, Class<T> singleMessageType) {
         // message 是 Chain 时
         if (message instanceof MessageChain) {
             MessageChain messageChain = (MessageChain) message;
-            // TODO:止步于此
-            return List.of(
-                    messageChain.stream().filter(
-                            singleMessage -> (singleMessage instanceof T)).map(T.class::cast
-                    ).toArray(T[]::new)
-            );
+            List<T> messageList = new ArrayList<T>();
+            messageChain.stream()
+                    .filter(
+                            singleMessage -> (singleMessage.getClass() == (singleMessageType))
+                    )
+                    .map(singleMessageType::cast)
+                    .forEach(messageList::add);
+            return messageList;
         }
         // message 是 SingleMessage时
-        if(message instanceof PlainText){
-            return List.of((PlainText) message);
-        }else{
-            logger.warn("Cannot Extract PlainText from message: "+message);
+        if (message.getClass() == singleMessageType) {
+            return List.of((T) message);
+        } else {
+            logger.warn("Cannot Extract PlainText from message: " + message);
             return null;
         }
     }
--- src/test/java/qingzhixing/utilities/MessageToolTest.java
+++ src/test/java/qingzhixing/utilities/MessageToolTest.java
@@ -1,5 +1,6 @@
 package qingzhixing.utilities;
 
+import net.mamoe.mirai.message.data.At;
 import net.mamoe.mirai.message.data.MessageChain;
 import net.mamoe.mirai.message.data.MessageChainBuilder;
 import org.junit.jupiter.api.Test;
@@ -15,7 +16,20 @@ class MessageToolTest {
                 .append("456")
                 .build();
         var plainText = MessageTool.ExtractPlainText(messageChain);
+        System.out.println(plainText);
         assert(plainText!=null);
         assertEquals("123456", plainText.get(0).toString());
+    }
+
+    @Test
+    void extractSingleMessageType() {
+        MessageChain messageChain =new MessageChainBuilder()
+                .append("123")
+                .append("456")
+                .append(new At(1145141919))
+                .build();
+        var messages = MessageTool.ExtractSingleMessageType(messageChain, At.class);
+        System.out.println(messages);
+        assert(messages!=null);
     }
 }
